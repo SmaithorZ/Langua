b170c5c80986fbeffe57dc18c517df580e10e200
Mateus Araújo <smaithorz@gmail.com>
2025-05-31 13:21:57 -0300

    Add A1 and A2 Russian Texts, migrate, add to DB.

--- Langua/Migrations/20250531162110_AddA1A2RuTexts.Designer.cs
+++ Langua/Migrations/20250531162110_AddA1A2RuTexts.Designer.cs
@@ -3,6 +3,7 @@ using Langua.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Langua.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250531162110_AddA1A2RuTexts")]
+    partial class AddA1A2RuTexts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
