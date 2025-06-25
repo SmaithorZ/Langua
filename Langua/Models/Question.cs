37c653e9627f89d19ce931aec145340048f8dd7f
Mateus Araújo <smaithorz@gmail.com>
2025-06-25 09:35:14 -0300

    Add Question model for questions

---
+++ Langua/Models/Question.cs
@@ -0,0 +1,23 @@
+﻿namespace Langua.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+
+        public int TextId { get; set; }
+        public Text Text { get; set; }
+
+
+        public string Content { get; set; }
+
+        public string option1 { get; set; }
+        public string option2 { get; set; }
+        public string option3 { get; set; }
+        public string option4 { get; set; }
+
+
+        public int CorrectOption { get; set; }
+
+
+    }
+}
